735ad7fea72f518a8b6fa62a4b3e62acc2aea38e
Nathan Vasse <nathan.vasse@gmail.com>
2023-04-26 15:38:09 +0200

    Updates

---
+++ _next/static/x1Jdw7OBv_Vgh62t3ioAY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/782-6f094197744b9e3d.js","static/chunks/pages/index-6c651222ac2e0c38.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
